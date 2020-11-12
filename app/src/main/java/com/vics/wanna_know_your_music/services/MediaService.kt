dbf44ec6fe74bc89fbc4acadef105a11c224e84c
Seungwon <jshtmddnjs@naver.com>
2020-11-12 18:54:26 +0900

    mediaSessionCompat & sessionCallback

--- app/src/main/java/com/vics/wanna_know_your_music/services/MediaService.kt
+++ app/src/main/java/com/vics/wanna_know_your_music/services/MediaService.kt
@@ -1,33 +1,75 @@
 package com.vics.wanna_know_your_music.services
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.MediaDescriptionCompat
 import android.support.v4.media.session.MediaSessionCompat
-import androidx.annotation.Nullable
+import android.text.TextUtils
+import android.util.Log
 import androidx.media.MediaBrowserServiceCompat
 
 
 class MediaService : MediaBrowserServiceCompat() {
-
     companion object {
         private const val TAG = "MediaService"
     }
-    private val mSession: MediaSessionCompat? = null
+    private var mSession: MediaSessionCompat? = null
+    override fun onCreate() {
+        super.onCreate()
 
-    @Nullable
+        //Build the MediaSession
+        mSession = MediaSessionCompat(this, TAG)
+        mSession!!.setFlags(
+            MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS or  // https://developer.android.com/guide/topics/media-apps/mediabuttons#mediabuttons-and-active-mediasessions
+                    // Media buttons on the device
+                    // (handles the PendingIntents for MediaButtonReceiver.buildMediaButtonPendingIntent)
+                    MediaSessionCompat.FLAG_HANDLES_QUEUE_COMMANDS
+        ) // Control the items in the queue (aka playlist)
+        // See https://developer.android.com/guide/topics/media-apps/mediabuttons for more info on flags
+        mSession!!.setCallback(MediaSessionCallback())
+
+        // A token that can be used to create a MediaController for this session
+        sessionToken = mSession!!.sessionToken
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent) {
+        Log.d(TAG, "onTaskRemoved: stopped")
+        super.onTaskRemoved(rootIntent)
+        stopSelf()
+    }
+
+    override fun onDestroy() {
+        mSession!!.release()
+        Log.d(
+            TAG,
+            "onDestroy: MediaPlayerAdapter stopped, and MediaSession released"
+        )
+    }
+
     override fun onGetRoot(
         s: String,
         i: Int,
-        @Nullable bundle: Bundle?
+        bundle: Bundle?
     ): BrowserRoot? {
-        return null
+        if (s == applicationContext.packageName) {
+            // Allowed to browse media
+        }
+        return BrowserRoot("empty_media", null) // return no media
     }
 
     override fun onLoadChildren(
         s: String,
         result: Result<List<MediaBrowserCompat.MediaItem>>
     ) {
+        Log.d(TAG, "onLoadChildren: called: $s, $result")
+
+        //  Browsing not allowed
+        if (TextUtils.equals("empty_media", s)) {
+            result.sendResult(null)
+            return
+        }
+        result.sendResult(null) // return all available media
     }
 
     inner class MediaSessionCallback : MediaSessionCompat.Callback() {
@@ -73,8 +115,5 @@ class MediaService : MediaBrowserServiceCompat() {
     }
 
 }
-
-
-
 
 
